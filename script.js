2adeb5c293cfb843c6e789791fffa1c0cd9ff4e7
Naveenkumar123-hash <120244421+Naveenkumar123-hash@users.noreply.github.com>
2023-07-06 18:14:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	var firstName = form.elements.fname.value;
+    var lastName = form.elements.lname.value;
+    alert(firstName+lastName);
 }
