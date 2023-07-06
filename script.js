643d2e715eaec330a84aa69aaf337678687e6cd7
Naveenkumar123-hash <120244421+Naveenkumar123-hash@users.noreply.github.com>
2023-07-06 18:16:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ function getFormvalue() {
 	var form = document.getElementById('form1');
 	var firstName = form.elements.fname.value;
     var lastName = form.elements.lname.value;
-    alert(firstName+lastName);
+    alert(firstName+" "+lastName);
 }
